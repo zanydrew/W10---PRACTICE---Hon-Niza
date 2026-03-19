e95f1ddacd62684287eeec439fa18d49f5ffd1e7
zanydrew <nizahon00@gmail.com>
2026-03-19 09:05:26 +0700

    create .gitignore

---
+++ Start-code/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import Game from "./components/Game.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header gameName="Monster Slayer" />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
