52cad0e3250538ca0ece731d801adf00bd8665d0
zanydrew <nizahon00@gmail.com>
2026-03-19 10:48:36 +0700

    implement Log component

--- Start-code/src/components/Log.jsx
+++ Start-code/src/components/Log.jsx
@@ -1,16 +1,14 @@
-function Log(){
+function Log({logs : {isPlayer, isDamage, point}}){
     return (
         <section id="log" className="container">
         <h2>Battle Log</h2>
         <ul>
-            <li>
-                <span>Player</span>
-                <span> heals for <span className="log--heal">12</span></span>
+            {logs.map((log, index) => (
+                <li key={index}>
+                <span>{isPlayer? "Player" : "Monster"}</span>
+                <span>{isDamage? "attaked for" : "heals for"} <span className={isDamage? "log--damage" : "log--heal"}>{point}</span></span>
             </li>
-            <li>
-                <span>Monster</span>
-                <span> attaked for <span className="log--damage">45</span></span>
-            </li>
+            ))}
         </ul>
     </section>
     )
